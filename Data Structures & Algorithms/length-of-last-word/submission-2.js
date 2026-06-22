8ebc683157059855119501f8112c0fef3e286203
Vladyslav Liubchenko <vladliubchenko.dev@gmail.com>
2026-06-22 19:23:41 +0200

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLastWord(s) {
+        const splittedS = s.split(" ")
+
+        for (let i = splittedS.length - 1; i >= 0; i--) {
+            if (splittedS[i]) {
+                return splittedS[i].length
+            }
+        }
+    }
+}
